196f0ef5aacda072c6a805e0c5a84ac588d4224e
Sudhanshu Raheja <sudhanshu@leftshift.io>
2020-01-01 14:44:55 +0530

    [sudhanshu] merged iterator and objects

--- index.js
+++ index.js
@@ -1,7 +1,76 @@
-const objects = require("./objects");
-const Iterator = require("./iterator");
+class Iterator {
+  constructor(obj) {
+    this.obj = obj;
+    return this;
+  }
 
-module.exports = {
-  objects,
-  Iterator
-};
+  $(key, returnValue = false) {
+    if (Iterator.isArray(this.obj) || Iterator.isObject(this.obj)) {
+      const value = Iterator.hasKey(this.obj, key) ? this.obj[key] : undefined;
+      const object = new Iterator(value);
+      return returnValue ? object.value() : object;
+    }
+    const value = undefined;
+    const object = new Iterator(value);
+    return returnValue ? object.value() : object;
+  }
+
+  keys() {
+    if (Iterator.isArray(this.obj)) {
+      return this.obj;
+    }
+    if (Iterator.isObject(this.obj)) {
+      return Object.keys(this.obj);
+    }
+    return [];
+  }
+
+  string() {
+    return Iterator.isString(this.obj) ? this.obj.toString() : "";
+  }
+
+  value() {
+    return this.obj;
+  }
+
+  static isArray(obj) {
+    return Array.isArray(obj);
+  }
+
+  static isObject(obj) {
+    return typeof obj === "object" && !Iterator.isArray(obj) && obj !== null;
+  }
+
+  static isNumber(obj) {
+    return typeof obj === "number";
+  }
+
+  static isString(obj) {
+    return typeof obj === "string";
+  }
+
+  static isBoolean(obj) {
+    return typeof obj === "boolean";
+  }
+
+  static isNull(obj) {
+    return obj === null;
+  }
+
+  static isUndefined(obj) {
+    return obj === undefined;
+  }
+
+  static hasKey(obj, key) {
+    if (Iterator.isArray(obj)) {
+      return Iterator.isNumber(key) && obj.length > key;
+    }
+    if (Iterator.isObject(obj)) {
+      const keys = Object.keys(obj);
+      return keys.includes(key);
+    }
+    return false;
+  }
+}
+
+module.exports = Iterator;
--- index.test.js
+++ index.test.js
@@ -1,8 +1,145 @@
-const o = require("./index");
+const Iterator = require("./index");
 
-test("Test objectIterator", () => {
-  expect(o.objects.isArray([123])).toBe(true);
+describe("Test Iterator", () => {
+  test("test Iterator", () => {
+    let i = new Iterator(undefined);
+    expect(i.value()).toBe(undefined);
+    i = new Iterator(undefined);
+    expect(i.keys()).toEqual([]);
+    i = new Iterator(undefined);
+    expect(i.string()).toBe("");
+    i = new Iterator(undefined);
+    expect(i.$("key").value()).toBe(undefined);
+    i = new Iterator(undefined);
+    expect(i.$("key", true)).toBe(undefined);
+    i = new Iterator(undefined);
+    expect(i.$("key").keys()).toEqual([]);
+    i = new Iterator(undefined);
+    expect(i.$("key").string()).toBe("");
 
-  const i = new o.Iterator({ key: "abc" });
-  expect(i.$("key").value()).toEqual("abc");
+    i = new Iterator(null);
+    expect(i.value()).toBe(null);
+    i = new Iterator(null);
+    expect(i.keys()).toEqual([]);
+    i = new Iterator(null);
+    expect(i.string()).toBe("");
+    i = new Iterator(null);
+    expect(i.$("key").value()).toBe(undefined);
+    i = new Iterator(null);
+    expect(i.$("key", true)).toBe(undefined);
+    i = new Iterator(null);
+    expect(i.$("key").keys()).toEqual([]);
+    i = new Iterator(null);
+    expect(i.$("key").string()).toBe("");
+
+    i = new Iterator(123);
+    expect(i.value()).toBe(123);
+    i = new Iterator(123);
+    expect(i.keys()).toEqual([]);
+    i = new Iterator(123);
+    expect(i.string()).toBe("");
+    i = new Iterator(123);
+    expect(i.$("key").value()).toBe(undefined);
+    i = new Iterator(123);
+    expect(i.$("key", true)).toBe(undefined);
+    i = new Iterator(123);
+    expect(i.$("key").keys()).toEqual([]);
+    i = new Iterator(123);
+    expect(i.$("key").string()).toBe("");
+
+    i = new Iterator("abc");
+    expect(i.value()).toBe("abc");
+    i = new Iterator("abc");
+    expect(i.keys()).toEqual([]);
+    i = new Iterator("abc");
+    expect(i.string()).toBe("abc");
+    i = new Iterator("abc");
+    expect(i.$("key").value()).toBe(undefined);
+    i = new Iterator("abc");
+    expect(i.$("key", true)).toBe(undefined);
+    i = new Iterator("abc");
+    expect(i.$("key").keys()).toEqual([]);
+    i = new Iterator("abc");
+    expect(i.$("key").string()).toBe("");
+
+    i = new Iterator(["abc"]);
+    expect(i.value()).toEqual(["abc"]);
+    i = new Iterator(["abc"]);
+    expect(i.keys()).toEqual(["abc"]);
+    i = new Iterator(["abc"]);
+    expect(i.string()).toBe("");
+    i = new Iterator(["abc"]);
+    expect(i.$("key").value()).toBe(undefined);
+    i = new Iterator(["abc"]);
+    expect(i.$("key", true)).toBe(undefined);
+    i = new Iterator(["abc"]);
+    expect(i.$("key").keys()).toEqual([]);
+    i = new Iterator(["abc"]);
+    expect(i.$("key").string()).toBe("");
+
+    i = new Iterator({ key: "abc" });
+    expect(i.value()).toEqual({ key: "abc" });
+    i = new Iterator({ key: "abc" });
+    expect(i.keys()).toEqual(["key"]);
+    i = new Iterator({ key: "abc" });
+    expect(i.string()).toBe("");
+    i = new Iterator({ key: "abc" });
+    expect(i.$("key").value()).toBe("abc");
+    i = new Iterator({ key: "abc" });
+    expect(i.$("key", true)).toBe("abc");
+    i = new Iterator({ key: "abc" });
+    expect(i.$("key").keys()).toEqual([]);
+    i = new Iterator({ key: "abc" });
+    expect(i.$("key").string()).toBe("abc");
+  });
+});
+
+describe("Test Objects", () => {
+  test("Check isArray", () => {
+    expect(Iterator.isArray(undefined)).toBe(false);
+    expect(Iterator.isArray(null)).toBe(false);
+    expect(Iterator.isArray(123)).toBe(false);
+    expect(Iterator.isArray("asdf")).toBe(false);
+    expect(Iterator.isArray([])).toBe(true);
+    expect(Iterator.isArray({})).toBe(false);
+  });
+
+  test("Check isObject", () => {
+    expect(Iterator.isObject(undefined)).toBe(false);
+    expect(Iterator.isObject(null)).toBe(false);
+    expect(Iterator.isObject(123)).toBe(false);
+    expect(Iterator.isObject("asdf")).toBe(false);
+    expect(Iterator.isObject([])).toBe(false);
+    expect(Iterator.isObject({})).toBe(true);
+  });
+
+  test("Check isNumber", () => {
+    expect(Iterator.isNumber(undefined)).toBe(false);
+    expect(Iterator.isNumber(null)).toBe(false);
+    expect(Iterator.isNumber(123)).toBe(true);
+    expect(Iterator.isNumber("asdf")).toBe(false);
+    expect(Iterator.isNumber([])).toBe(false);
+    expect(Iterator.isNumber({})).toBe(false);
+  });
+
+  test("Check isString", () => {
+    expect(Iterator.isString(undefined)).toBe(false);
+    expect(Iterator.isString(null)).toBe(false);
+    expect(Iterator.isString(123)).toBe(false);
+    expect(Iterator.isString("asdf")).toBe(true);
+    expect(Iterator.isString([])).toBe(false);
+    expect(Iterator.isString({})).toBe(false);
+  });
+
+  test("Check hasKey", () => {
+    expect(Iterator.hasKey(undefined, "abc")).toBe(false);
+    expect(Iterator.hasKey(null, "abc")).toBe(false);
+    expect(Iterator.hasKey(123, "abc")).toBe(false);
+    expect(Iterator.hasKey("asdf", "abc")).toBe(false);
+    expect(Iterator.hasKey([], "abc")).toBe(false);
+    expect(Iterator.hasKey(["abc"], 0)).toBe(true);
+    expect(Iterator.hasKey(["abc"], 1)).toBe(false);
+    expect(Iterator.hasKey({ abc: "def" }, "abc")).toBe(true);
+    expect(Iterator.hasKey({ abc: "def" }, "abc1")).toBe(false);
+  });
 });
